3d3afd478ac528175317d03bb1c45d8c1036e6fd
Priscila Tischer <pryscilaticher@hotmail.com>
2025-08-04 07:34:59 -0300

    Create 01_ola_mundo.py
    Adiciona exercício inicial "Olá, Mundo!" do Módulo 1

---
+++ modulo-01/01_ola_mundo.py
@@ -0,0 +1,17 @@
+# Módulo 01 - Exercício 01: Olá, Mundo!
+
+"""
+Anotação Pessoal:
+
+O "Olá, Mundo!" é um rito de passagem tradicional ao iniciar o aprendizado
+de uma nova linguagem de programação.
+
+Mesmo já tendo tido contato com Python na Universidade, decidi fazer este curso
+no SENAI para solidificar minhas bases. Acredito que, no fim, aprender é
+isso: quanto mais se aprende, mais a gente vê o quanto ainda precisa aprender.
+
+Este código simples representa o primeiro passo de um aprofundamento
+consciente e dedicado.
+"""
+
+print("Olá, Mundo!")
